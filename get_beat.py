b2b390c3f24934b611bcf10289674ebe8b64ea7b
johnd <john@tvplus.com>
2012-09-28 18:21:39 -0700

    removed averaging code and replaced with exponential decay, much easier to calculate

--- get_beat.py
+++ get_beat.py
@@ -2,37 +2,17 @@
 import struct
 import sys, time
 
-
-from collections import deque
- 
-class Simplemovingaverage():
-    def __init__(self, period):
-        assert period == int(period) and period > 0, "Period must be an integer >0"
-        self.period = period
-        self.stream = deque()
- 
-    def __call__(self, n):
-        stream = self.stream
-        stream.append(n)    # appends on the right
-        streamlength = len(stream)
-        if streamlength > self.period:
-            stream.popleft()
-            streamlength -= 1
-        if streamlength == 0:
-            average = 0
-        else:
-            average = sum( stream ) / streamlength
- 
-        return average
+def expDecay(newValue, workingAverage, smoothingFactor):
+	return (newValue*smoothingFactor) + ( workingAverage * ( 1.0 - smoothingFactor))
+	
 f = sys.stdin
 #f = open('beats.wav')
 byte = f.read(2)
 pos = 0
-smoothing  = 1.5
 v=0
-smaInstant = Simplemovingaverage(500)
-smaLocal = Simplemovingaverage(8000)
 bpm = 0
+inst = 0
+local = 0
 decay = 0
 start = time.time()
 while byte != "":
@@ -40,17 +20,13 @@ while byte != "":
 	#v += (struct.unpack('<h', byte)[0] - v) / smoothing
 	v = abs(struct.unpack('<h', byte)[0])
 
-	inst = smaInstant(v)	
-	local = smaLocal(v)	
-	if False:# pos % 1000 == 0:
-		print 'inst',inst
-		print 'local',local
-		print 'ratio', inst/local
+	inst = expDecay(v, inst, .01)
+	local = expDecay(v, local, .001)
 	try:
 		if (inst/float(local)) > 2 and decay <= 0:
 			bpm += 1
 			print '------beat-----'
-			decay = 6000
+			decay = 3000
 	except:
 		pass
 	decay -= 1
