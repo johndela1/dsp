a2d6de1f2b92eeec62344b553c86ed9eaca7ec9b
johnd <john@tvplus.com>
2012-09-29 23:39:47 -0700

    changes variable names

--- get_beat.py
+++ get_beat.py
@@ -1,39 +1,40 @@
 #!/usr/bin/python -u
+
 import struct
 import sys, time
 
 def expDecay(newValue, workingAverage, smoothingFactor):
-	return (newValue*smoothingFactor) + ( workingAverage * ( 1.0 - smoothingFactor))
+	return ((newValue*smoothingFactor) +
+		( workingAverage * ( 1.0 - smoothingFactor)))
+
 	
 f = sys.stdin
-#f = open('beats.wav')
 byte = f.read(2)
 pos = 0
 v=0
 bpm = 0
-inst = 0
-local = 0
+instEnergy = 0
+avgEnergy = 0
 decay = 0
 start = time.time()
 while byte != "":
 	pos += 1
-	#v += (struct.unpack('<h', byte)[0] - v) / smoothing
 	v = abs(struct.unpack('<h', byte)[0])
-
-	inst = expDecay(v, inst, .01)
-	local = expDecay(v, local, .001)
+	#if pos % 10000 == 0: print v
+	instEnergy = expDecay(v, instEnergy, .01)
+	avgEnergy = expDecay(v, avgEnergy, .001)
 	try:
-		if (inst/float(local)) > 2 and decay <= 0:
+		if (instEnergy/float(avgEnergy)) > 2 and decay <= 0:
 			bpm += 1
 			print '------beat-----'
-			decay = 3000
+			decay = 800
 	except:
 		pass
 	decay -= 1
 	byte = f.read(2)
 
-	if pos % 32000 == 0:
-		print 'fps: ', (8000 / (time.time() - start))
+	if False:#pos % 32000 == 0:
+		print 'samples/sec:', (32000 / (time.time() - start))
 		print 'bpm', bpm *15
 		bpm = 0
 		start = time.time()
