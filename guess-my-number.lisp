e3226f0a740d0cfa0a2947c8a70c621d3b925450
johnd <john@tvplus.com>
2013-04-01 11:42:30 -0700

    added guess-my-number for basic lisp sample

---
+++ guess-my-number.lisp
@@ -0,0 +1,25 @@
+#!/usr/bin/clisp
+
+(defparameter *small* 1)
+(defparameter *big* 100)
+
+(defun guess-my-number ()
+ (ash (+ *big* *small*) -1))
+
+(defun smaller ()
+ (setf *big* (1- (guess-my-number)))
+ (guess-my-number))
+
+(defun bigger ()
+ (setf *small* (1+ (guess-my-number)))
+ (guess-my-number))
+
+(defun start-over ()
+ (defparameter *small* 1)
+ (defparameter *big* 100)
+ (guess-my-number))
+
+(defun game-repl ()
+ (loop (print (eval (read)))))
+
+(game-repl)
